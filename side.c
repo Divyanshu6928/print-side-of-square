bb1079a4ceba3f00f4869e518e7c250766713cbb
Divyanshu6928 <148749004+Divyanshu6928@users.noreply.github.com>
2023-10-26 22:29:46 +0530

    Add files via upload

---
+++ side.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int side;
+	printf("Enter the side of square :\n");
+	scanf("%d",&side);
+	printf("\nArea of square is:%d\n",side*side);
+	printf("Perimeter of square is:%d",4*side);
+}
